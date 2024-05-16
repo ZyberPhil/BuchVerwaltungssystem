b61e3aa9ad6d6127e92a257f09fcf4ae4ce0e494
Phil Hempel <philhempel4@gmail.com>
2024-05-16 19:41:08 +0200

    Donnerstag Hausaufgaben SWE [16.05.2024]

---
+++ Buch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+public class Buch
+{
+    public string Title { get; set; }
+    public string Autor { get; set; }
+    public string ISBN { get; set; }
+    public DateTime Erscheinnungsjahr { get; set; }
+
+    public Buch(string title, string autor, string isbn, DateTime erscheinnungsjahr)
+    {
+        Title = title;
+        Autor = autor;
+        ISBN = isbn;
+        Erscheinnungsjahr = erscheinnungsjahr;
+    }
+
+    public void Hinzufügen(string title, string autor, string isbn, DateTime erscheinnungsjahr)
+    {
+
+    }
+
+    public void Entfernen(string title, string autor, string isbn, DateTime erscheinnungsjahr)
+    {
+
+    }
+
+    public void Anzeigen(string title, string autor, string isbn, DateTime erscheinnungsjahr)
+    {
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace BuckVerwaltungssystem
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
